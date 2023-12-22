4e3c5c2c57434653ba519c2d3ee792629fa1c80a
Eric Sizemore <esizemore05@gmail.com>
2023-12-22 13:57:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,160 @@
+# LibrariesIO - A simple API wrapper/client for the Libraries.io API.
+
+* IN DEVELOPMENT: Not considered production ready. * as of 12/22/2023
+
+[LibrariesIO](http://github.com/ericsizemore/librariesio/) is a small collection of useful functions that are aimed to make developers' lives just a tad bit easier.
+
+## Important Note
+
+This project was born from the desire to expand my knowledge of API's and GuzzleHttp. My implementation is far from perfect, so I am open to any and all feedback that one may wish to provide.
+
+* The Libraries.io API has the ability for pagination, however it is not yet fully implemented in this library.
+* The `subscriptions` endpoint (adding, updating, or deleting subscriptions) is not yet implemented.
+
+## Installation
+
+### Composer
+
+Install the latest version with:
+
+```bash
+$ composer require esi/librariesio
+```
+
+Then, within your project (if not already included), include composer's autoload. For example:
+
+```php
+<?php
+
+require 'vendor/autoload.php';
+
+?>
+```
+
+## Basic Usage
+
+LibrariesIO splits the different endpoints of the API into several "components":
+
+  * Esi\LibrariesIO\Platform\Platform
+  * Esi\LibrariesIO\Project
+    * Esi\LibrariesIO\Project\Contributors
+    * Esi\LibrariesIO\Project\Dependencies
+    * Esi\LibrariesIO\Project\Dependents
+    * Esi\LibrariesIO\Project\DependentRepositories
+    * Esi\LibrariesIO\Project\Project
+    * Esi\LibrariesIO\Project\Search
+    * Esi\LibrariesIO\Project\SourceRank
+  * Esi\LibrariesIO\Repository
+    * Esi\LibrariesIO\Repository\Dependencies
+    * Esi\LibrariesIO\Repository\Projects
+    * Esi\LibrariesIO\Repository\Repository
+  * Esi\LibrariesIO\User
+    * Esi\LibrariesIO\User\Dependencies
+    * Esi\LibrariesIO\User\Packages
+    * Esi\LibrariesIO\User\PackageContributions
+    * Esi\LibrariesIO\User\Repositories
+    * Esi\LibrariesIO\User\RepositoryContributions
+    * Esi\LibrariesIO\User\Subscriptions
+    * Esi\LibrariesIO\User\User
+
+As an example, let's say you want to get a list of the available platforms. To do so:
+
+```php
+<?php
+
+use Esi\LibrariesIO\Platform\Platform;
+
+$api = new Platform('..yourapikey..', \sys_get_temp_dir());
+$response = $api->makeRequest();
+
+print_r($api->getJson($response));
+
+/*
+Array
+(
+    [0] => Array
+        (
+            [name] => NPM
+            [project_count] => 4079049
+            [homepage] => https://www.npmjs.com
+            [color] => #f1e05a
+            [default_language] => JavaScript
+        )
+
+    [1] => Array
+        (
+            [name] => Maven
+            [project_count] => 588275
+            [homepage] => http://maven.org
+            [color] => #b07219
+            [default_language] => Java
+        )
+    [...]
+)
+*/
+?>
+```
+
+## Documentation
+
+*WORK IN PROGRESS*
+
+## About
+
+### Requirements
+
+- LibrariesIO works with PHP 8.2.0 or above.
+
+### Submitting bugs and feature requests
+
+Bugs and feature requests are tracked on [GitHub](https://github.com/ericsizemore/librariesio/issues)
+
+Issues are the quickest way to report a bug. If you find a bug or documentation error, please check the following first:
+
+* That there is not an Issue already open concerning the bug
+* That the issue has not already been addressed (within closed Issues, for example)
+
+### Contributing
+
+LibrariesIO accepts contributions of code and documentation from the community. 
+These contributions can be made in the form of Issues or [Pull Requests](http://help.github.com/send-pull-requests/) on the [LibrariesIO repository](https://github.com/ericsizemore/librariesio).
+
+LibrariesIO is licensed under the MIT license. When submitting new features or patches to LibrariesIO, you are giving permission to license those features or patches under the MIT license.
+
+LibrariesIO tries to adhere to PHPStan level 9 with strict rules and bleeding edge. Please ensure any contributions do as well.
+
+#### Guidelines
+
+Before we look into how, here are the guidelines. If your Pull Requests fail to pass these guidelines it will be declined, and you will need to re-submit when you’ve made the changes. This might sound a bit tough, but it is required for me to maintain quality of the code-base.
+
+#### PHP Style
+
+Please ensure all new contributions match the [PSR-12](https://www.php-fig.org/psr/psr-12/) coding style guide. The project is not fully PSR-12 compatible, yet; however, to ensure the easiest transition to the coding guidelines, I would like to go ahead and request that any contributions follow them.
+
+#### Documentation
+
+If you change anything that requires a change to documentation then you will need to add it. New methods, parameters, changing default values, adding constants, etc. are all things that will require a change to documentation. The change-log must also be updated for every change. Also, PHPDoc blocks must be maintained.
+
+##### Documenting functions/variables (PHPDoc)
+
+Please ensure all new contributions adhere to:
+
+  * [PSR-5 PHPDoc](https://github.com/php-fig/fig-standards/blob/master/proposed/phpdoc.md)
+  * [PSR-19 PHPDoc Tags](https://github.com/php-fig/fig-standards/blob/master/proposed/phpdoc-tags.md)
+
+When documenting new functions, or changing existing documentation.
+
+#### Branching
+
+One thing at a time: A pull request should only contain one change. That does not mean only one commit, but one change - however many commits it took. The reason for this is that if you change X and Y but send a pull request for both at the same time, we might really want X but disagree with Y, meaning we cannot merge the request. Using the Git-Flow branching model you can create new branches for both of these features and send two requests.
+
+### Author
+
+Eric Sizemore - <admin@secondversion.com> - <https://www.secondversion.com>
+
+### License
+
+LibrariesIO is licensed under the MIT License - see the `LICENSE` file for details
+
+
+[![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2Fericsizemore%2Flibrariesio.svg?type=large)](https://app.fossa.com/projects/git%2Bgithub.com%2Fericsizemore%2Flibrariesio?ref=badge_large)
---
+++ src/AbstractBase.php
@@ -0,0 +1,212 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * LibrariesIO - A simple API wrapper/client for the Libraries.io API.
+ *
+ * @author    Eric Sizemore <admin@secondversion.com>
+ * @package   LibrariesIO
+ * @link      https://www.secondversion.com/
+ * @version   1.0.0
+ * @copyright (C) 2023 Eric Sizemore
+ * @license   The MIT License (MIT)
+ */
+namespace Esi\LibrariesIO;
+
+// Exceptions and Attributes
+use GuzzleHttp\Exception\GuzzleException;
+use SensitiveParameter;
+use InvalidArgumentException;
+use JsonException;
+
+// Guzzle
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Exception\ClientException;
+use Psr\Http\Message\ResponseInterface;
+
+// Cache
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
+use Kevinrob\GuzzleCache\Storage\Psr6CacheStorage;
+
+// Functions and constants
+use function is_dir, is_writable, json_decode, preg_match;
+use const JSON_THROW_ON_ERROR;
+
+/**
+ * LibrariesIO - A simple API wrapper/client for the Libraries.io API.
+ *
+ * @author    Eric Sizemore <admin@secondversion.com>
+ * @package   LibrariesIO
+ * @link      https://www.secondversion.com/
+ * @version   1.0.0
+ * @copyright (C) 2023 Eric Sizemore
+ * @license   The MIT License (MIT)
+ *
+ * Copyright (C) 2023 Eric Sizemore. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+abstract class AbstractBase
+{
+    /**
+     * GuzzleHttp Client
+     *
+     * @var Client
+     */
+    protected Client $client;
+
+    /**
+     * Base API endpoint.
+     *
+     * @var string
+     */
+    protected const string API_URL = 'https://libraries.io/api/';
+
+    /**
+     * Libraries.io API key.
+     *
+     * @see https://libraries.io/account
+     * @var ?string
+     */
+    protected ?string $apiKey = null;
+
+    /**
+     * Path to your cache folder on the file system.
+     *
+     * @var ?string
+     */
+    protected ?string $cachePath = null;
+
+    /**
+     * Constructor.
+     *
+     * @param string  $apiKey    Your Libraries.io API Key
+     * @param ?string $cachePath The path to your cache on the filesystem
+     */
+    public function __construct(
+        #[SensitiveParameter]
+        string $apiKey,
+        ?string $cachePath = null
+    )
+    {
+        if (preg_match('/^[0-9a-fA-F]{32}$/', $apiKey) === 0) {
+            throw new InvalidArgumentException('API key appears to be invalid, keys are typically alpha numeric and 32 chars in length');
+        }
+
+        $this->apiKey = $apiKey;
+
+        if ($cachePath !== null && is_dir($cachePath) && is_writable($cachePath)) {
+            $this->cachePath = $cachePath;
+        }
+    }
+
+    /**
+     * Builds our GuzzleHttp client.
+     *
+     * @access protected
+     * @param  array<string, int|string> $query
+     * @return Client
+     */
+    protected function makeClient(?array $query = null): Client
+    {
+        // Some endpoints do not require any query parameters
+        if ($query === null) {
+            $query = [];
+        }
+
+        // Add the API key to the query
+        $query['api_key'] = $this->apiKey; 
+
+        // Client options
+        $options = [
+            'base_uri' => self::API_URL,
+            'query'    => $query,
+            'headers' => [
+                'Accept' => 'application/json'
+            ],
+            'http_errors' => true
+        ];
+
+        // If we have a cache path, create our Cache handler
+        if ($this->cachePath !== null) {
+            // Create default HandlerStack
+            $stack = HandlerStack::create();
+
+            // Add this middleware to the top with `push`
+            $stack->push(new CacheMiddleware(
+                new PrivateCacheStrategy(
+                    new Psr6CacheStorage(
+                        new FilesystemAdapter('libio', 60, $this->cachePath)
+                    )
+                )
+            ), 'cache');
+
+            // Add handler to $options
+            $options += ['handler' => $stack];
+        }
+
+        // Build client
+        $this->client = new Client($options);
+
+        return $this->client;
+    }
+
+    /**
+     * Performs the actual request to the API.
+     *
+     * @param array<string, int|string> $options
+     * @return ResponseInterface
+     * @throws ClientException|GuzzleException
+     */
+    abstract function makeRequest(array $options): ResponseInterface;
+
+    /**
+     * Decodes the jSON returned from the API. Returns as an associative array.
+     *
+     * @param ResponseInterface $response The response object from makeRequest()
+     * @return array<mixed>
+     * @throws JsonException
+     */
+    public function toArray(ResponseInterface $response): array
+    {
+        /** @var array<mixed> $json **/
+        $json = json_decode($response->getBody()->getContents(), true, flags: JSON_THROW_ON_ERROR);
+
+        return $json;
+    }
+
+    /**
+     * Decodes the jSON returned from the API. Returns as an array of objects.
+     *
+     * @param ResponseInterface $response The response object from makeRequest()
+     * @return array<mixed>
+     * @throws JsonException
+     */
+    public function toObject(ResponseInterface $response): array
+    {
+        /** @var array<mixed> $json **/
+        $json = json_decode($response->getBody()->getContents(), false, flags: JSON_THROW_ON_ERROR);
+        
+        return $json;
+    }
+}
